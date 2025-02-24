a7d2171ee9dfc52aafff2493f803dca37848f4c0
Jahidur Rahman <128843051+mdjahidurjr99@users.noreply.github.com>
2025-02-24 20:07:43 +0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use(hpp());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://lastassignment.vercel.app/", credentials: true }));
 
 
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 3000 });
